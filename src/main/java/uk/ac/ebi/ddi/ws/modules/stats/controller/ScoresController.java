3332bb510cb939780fe60831e06ec21881c7180c
¨gaurhari¨ <gaurharidass@gmail.com>
2017-09-08 16:32:33 +0100

    added api for viewcount of datasets

--- src/main/java/uk/ac/ebi/ddi/ws/modules/stats/controller/ScoresController.java
+++ src/main/java/uk/ac/ebi/ddi/ws/modules/stats/controller/ScoresController.java
@@ -6,20 +6,29 @@ import com.wordnik.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import uk.ac.ebi.ddi.ebe.ws.dao.model.europmc.Citation;
+import uk.ac.ebi.ddi.service.db.model.dataset.MostAccessedDatasets;
 import uk.ac.ebi.ddi.service.db.model.similarity.Citations;
 import uk.ac.ebi.ddi.service.db.model.similarity.EBISearchPubmedCount;
 import uk.ac.ebi.ddi.service.db.model.similarity.ReanalysisData;
+import uk.ac.ebi.ddi.service.db.service.dataset.IMostAccessedDatasetService;
 import uk.ac.ebi.ddi.service.db.service.similarity.CitationService;
 import uk.ac.ebi.ddi.service.db.service.similarity.EBIPubmedSearchService;
 import uk.ac.ebi.ddi.service.db.service.similarity.ReanalysisDataService;
 import uk.ac.ebi.ddi.ws.modules.dataset.controller.DatasetController;
+import uk.ac.ebi.ddi.ws.modules.dataset.model.DataSetResult;
+import uk.ac.ebi.ddi.ws.modules.dataset.model.DatasetSummary;
 import uk.ac.ebi.ddi.ws.modules.dataset.model.OmicsDataset;
 import uk.ac.ebi.ddi.ws.util.Constants;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by gaur on 29/08/17.
@@ -39,6 +48,9 @@ public class ScoresController {
 
     @Autowired
     private ReanalysisDataService reanalysisDataService;
+
+    @Autowired
+    private IMostAccessedDatasetService mostAccessedDatasetService;
 
     @ApiOperation(value = "Retrieve an Specific Dataset Citation Count", position = 1, notes = "Retrieve an specific dataset citation count")
     @RequestMapping(value = "/citation/{source}/{acc}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
@@ -76,4 +88,21 @@ public class ScoresController {
         String database = Constants.Database.retriveAnchorName(source);
         return reanalysisDataService.getReanalysisCount(acc,database);
     }
+
+
+    @ApiOperation(value = "views of specific dataset", position = 1, notes = "Retrieve an specific dataset view count")
+    @RequestMapping(value = "/views/{source}/{acc}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK) // 200
+    public @ResponseBody
+    int getDatasetViews(
+            @ApiParam(value = "Accession of the Dataset in the resource, e.g : 10090109")
+            @PathVariable(value = "acc") String acc,
+            @ApiParam(value = "source, e.g: paxdb")
+            @PathVariable(value = "source") String source)
+    {
+        String database = Constants.Database.retriveAnchorName(source);
+        MostAccessedDatasets datasets = mostAccessedDatasetService.getDatasetView(acc,database);
+        return datasets.getTotal();
+    }
+
 }
