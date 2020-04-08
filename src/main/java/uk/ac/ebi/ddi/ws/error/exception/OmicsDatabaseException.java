321ad4d5a47d96d827d243304aadc3c8a0afbd1c
Gaurhari <gaurharidass@gmail.com>
2020-04-08 22:29:05 +0100

    Added database custom exception

---
+++ src/main/java/uk/ac/ebi/ddi/ws/error/exception/OmicsDatabaseException.java
@@ -0,0 +1,16 @@
+package uk.ac.ebi.ddi.ws.error.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.NOT_FOUND,
+        reason = "Database is not available, Please provide correct database.")
+public class OmicsDatabaseException extends RuntimeException {
+    public OmicsDatabaseException(String message) {
+        super(message, null, true, true);
+    }
+
+    public OmicsDatabaseException(String message, Throwable cause) {
+        super(message, cause, true, true);
+    }
+}
