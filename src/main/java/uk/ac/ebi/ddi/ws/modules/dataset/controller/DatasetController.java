18fd0a2147bf5d12ba5ed65ee7c60e0a7dddedbd
Gaurhari <gaurharidass@gmail.com>
2021-05-06 10:45:12 +0100

    updated compile issue

--- src/main/java/uk/ac/ebi/ddi/ws/modules/dataset/controller/DatasetController.java
+++ src/main/java/uk/ac/ebi/ddi/ws/modules/dataset/controller/DatasetController.java
@@ -16,7 +16,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+//import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -455,7 +455,7 @@ public class DatasetController {
         datasetDetail = WsUtilities.mapSimilarsToDatasetDetails(datasetDetail, similars);
 
         //404 exception
-        if(datasetDetail. getId() == null) {
+        if (datasetDetail.getId() == null) {
             throw new ResourceNotFoundException("Dataset not found");
         }
         return datasetDetail;
