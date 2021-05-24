d3244fa4a963e310a8754f8fd85590287a00ed7a
Gaurhari <gaurharidass@gmail.com>
2021-05-25 00:32:19 +0100

    updated 404 exception

--- src/main/java/uk/ac/ebi/ddi/ws/error/exception/ResourceNotFoundException.java
+++ src/main/java/uk/ac/ebi/ddi/ws/error/exception/ResourceNotFoundException.java
@@ -1,9 +1,14 @@
 package uk.ac.ebi.ddi.ws.error.exception;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * @author Jose A. Dianes <jdianes@ebi.ac.uk>
  *
  */
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Dataset not found.")
 public class ResourceNotFoundException extends RuntimeException {
 
     public ResourceNotFoundException(String message) {
